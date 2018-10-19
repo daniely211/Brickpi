4d7c906d9106daea98ec962a01a9a0e301f10ba2
Daniel Yung <danielyung211@gmail.com>
2018-10-19 17:43:08 +0100

    change kd

--- graph.py
+++ graph.py
@@ -8,7 +8,7 @@ y_points_motor2 = []
 
 periods = []
 
-with open('log700.0', 'r') as tsvin:
+with open('log kp:300.0 ki:400.0 kd:0.0', 'r') as tsvin:
 	tsvin = csv.reader(tsvin, delimiter='\t')
 
 	i = 0
