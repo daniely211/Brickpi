8564ff614f9b30a10a94fb30dbc05ff96ee62839
wmvndrscht <wv116@ic.ac.uk>
2018-10-29 16:13:16 +0000

    moving backwards

--- moveNew.py
+++ moveNew.py
@@ -45,15 +45,15 @@ def forward(dist):
     while not interface.motorAngleReferencesReached(motors) :
         try:
             motorAngles = interface.getMotorAngles(motors)
-        	if motorAngles :
+            if motorAngles :
         		# print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
         		# pause execution for 0.1 seconds
-        		time.sleep(0.1)
+              time.sleep(0.1)
         except Exception as e:
         	print("Exception!!!")
         	print("stop logging")
         	interface.stopLogging()
         	raise e
 
-forward(10)
+forward(-10)
 interface.terminate()
