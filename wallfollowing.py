47003fbf95b8da55484acfe08f95973dbb024b2d
Daniel Yung <danielyung211@gmail.com>
2018-10-30 10:58:13 +0000

    sonar test

--- wallfollowing.py
+++ wallfollowing.py
@@ -4,20 +4,39 @@ import time
 interface=brickpi.Interface()
 interface.initialize()
 
-port = 0 # port which ultrasoic sensor is plugged in to
+motors = [0,3]
+speed = 6.0
 
+interface.motorEnable(motors[0])
+interface.motorEnable(motors[1])
+
+motorParams = interface.MotorAngleControllerParameters()
+motorParams.maxRotationAcceleration = 6.0
+motorParams.maxRotationSpeed = 12.0
+motorParams.feedForwardGain = 255/20.0
+motorParams.minPWM = 18.0
+motorParams.pidParameters.minOutput = -255
+motorParams.pidParameters.maxOutput = 255
+motorParams.pidParameters.k_p = 540
+motorParams.pidParameters.k_i = 2000
+motorParams.pidParameters.K_d = 34
+interface.setMotorAngleControllerParameters(motors[0],motorParams)
+interface.setMotorAngleControllerParameters(motors[1],motorParams)
+
+port = 0 # port which ultrasoic sensor is plugged in to
 interface.sensorEnable(port, brickpi.SensorType.SENSOR_ULTRASONIC);
 
-kp = 100
+kp = 2
 zdesired = 30
 while True:
 	zactual = interface.getSensorValue(port)
 
 	if zactual :
         # calculate the speed at which it needs to go at
-        # v = −kp (zdesired − zactual )
 
         speed = -kp*(zdesired - zactual)
+        interface.setMotorRotationSpeedReferences(motors,[speed,speed])
+
         print "speed"+ str(speed)
 		print zactual
 	else:
