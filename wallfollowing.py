2ec5cebe4b46ea2a2d90e7520b43774f24f03afe
Daniel Yung <danielyung211@gmail.com>
2018-10-30 10:48:49 +0000

    wall following printing

---
+++ wallfollowing.py
@@ -0,0 +1,28 @@
+import brickpi
+import time
+
+interface=brickpi.Interface()
+interface.initialize()
+
+port = 0 # port which ultrasoic sensor is plugged in to
+
+interface.sensorEnable(port, brickpi.SensorType.SENSOR_ULTRASONIC);
+
+kp = 100
+zdesired = 30
+while True:
+	zactual = interface.getSensorValue(port)
+
+	if zactual :
+        # calculate the speed at which it needs to go at
+        # v = −kp (zdesired − zactual )
+
+        speed = -kp*(zdesired - zactual)
+        print "speed"+ str(speed)
+		print zactual
+	else:
+		print "Failed US reading"
+
+	time.sleep(0.05)
+
+interface.terminate()
