9e7066d5afefc2c4b80eac1d0a3c1aaa3d23a318
Toma <tp2116@ic.ac.uk>
2018-10-31 14:05:20 +0000

    added missing returns in square_with_particles and cleaned up a bit

--- square_with_particles.py
+++ square_with_particles.py
@@ -1,6 +1,6 @@
 import brickpi
 import time
-import math
+from math import cos,sin 
 from move import forward,left, right
 import random
 
@@ -27,19 +27,22 @@ motorParams.pidParameters.K_d = 34
 current =  (0,0,0)
 
 def generate_particles_from_movement(current, D):
+      x,y,theta = current[0], current[1], current[2]
       new_points = []
       for i in range(100):
             e = random.gauss(0, 5)
             f = random.gauss(0, 5)
-            new_point = (current[0] + (D + e)*math.cos(current[2]), current[1] + (D + e)*math.sin(current[2]), current[2] + f)
+            new_point = (x + (D + e)*cos(theta), y + (D + e)*sin(theta), theta + f)
             new_points.append(new_point)
-
+      return new_points
 def generate_particles_from_turn(current, angle):
+      x,y,theta = current[0], current[1], current[2]
       new_points = []
       for i in range(100):
             g = random.gauss(0,5)
-            new_point = (current[0], current[1], current[2] + angle + g)
+            new_point = (x, y, theta + angle + g)
             new_points.append(new_point)
+      return new_points
 
 def square():
 	#18.6046511628
@@ -53,7 +56,7 @@ def square():
                   avgY = sum([y for (x,y,theta) in particles])/100
                   avgTheta = sum([theta for (x,y,theta) in particles])/100
                   current = (avgX, avgY,avgTheta)
-                  line = (previous_pos[0], previous_pos[1], current[0], current[1])
+                  line = (previous_pos[0], previous_pos[1], avgX, avgY)
                   #plot the points
                   print("drawLine:" + str(line))
                   print("drawParticles:" + str(particles))
